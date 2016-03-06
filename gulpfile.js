1175126fb673de29bc46262d4bd74e3565105745
Alex5646 <catia.petrowi4@yandex.ru>
2016-03-06 18:42:11 +1000

    Inint

---
+++ gulpfile.js
@@ -0,0 +1,89 @@
+var gulp         = require('gulp'),
+    sourcemaps   = require('gulp-sourcemaps'),
+    concat       = require('gulp-concat'),
+    uglify       = require('gulp-uglify'),
+    sass         = require('gulp-sass'),
+    cssmin       = require('gulp-cssmin'),
+    postcss      = require('gulp-postcss'),
+    autoprefixer = require('autoprefixer'),
+    plumber      = require('gulp-plumber');
+
+// Обьединение js и сжатие
+gulp.task('js', function() {
+    gulp
+        .src([
+            // JQuery UI Core
+            './assets/js/ui/core.js',
+            './assets/js/ui/form-reset-mixin.js',
+            './assets/js/ui/position.js',
+            './assets/js/ui/widget.js',
+
+            // JQuery UI Mouse
+            './assets/js/ui/mouse.js',
+            './assets/js/ui/draggable.js',
+            './assets/js/ui/droppable.js',
+            './assets/js/ui/resizable.js',
+            './assets/js/ui/selectable.js',
+            './assets/js/ui/sortable.js',
+
+            // JQuery UI Widgets
+            './assets/js/ui/button.js',
+            './assets/js/ui/accordion.js',
+            './assets/js/ui/autocomplete.js',
+            './assets/js/ui/datepicker.js',
+            './assets/js/ui/dialog.js',
+            './assets/js/ui/menu.js',
+            './assets/js/ui/progressbar.js',
+            './assets/js/ui/selectmenu.js',
+            './assets/js/ui/slider.js',
+            './assets/js/ui/spinner.js',
+            './assets/js/ui/tabs.js',
+            './assets/js/ui/tooltip.js',
+
+            // JQuery UI Effexts
+            './assets/js/ui/effect.js',
+            './assets/js/ui/effect-blind.js',
+            './assets/js/ui/effect-bounce.js',
+            './assets/js/ui/effect-clip.js',
+            './assets/js/ui/effect-drop.js',
+            './assets/js/ui/effect-explode.js',
+            './assets/js/ui/effect-fade.js',
+            './assets/js/ui/effect-fold.js',
+            './assets/js/ui/effect-highlight.js',
+            './assets/js/ui/effect-puff.js',
+            './assets/js/ui/effect-pulsate.js',
+            './assets/js/ui/effect-scale.js',
+            './assets/js/ui/effect-shake.js',
+            './assets/js/ui/effect-size.js',
+            './assets/js/ui/effect-slide.js',
+            './assets/js/ui/effect-transfer.js'
+        ])
+        .pipe(plumber())
+        .pipe(sourcemaps.init())
+        .pipe(concat('jquery-ui.js'))
+        //.pipe(uglify())
+        .pipe(sourcemaps.write('./'))
+        .pipe(gulp.dest('./assets/js'));
+});
+
+// Компиляция sass, обьединение css, сжатие
+gulp.task('css', function() {
+    gulp.src('./assets/sass/**/*.{sass,scss}')
+        .pipe(plumber())
+        .pipe(sourcemaps.init())
+        .pipe(sass())
+        .pipe(postcss([
+            autoprefixer({browsers: ['last 2 versions', '> 1%']})
+        ]))
+        //.pipe(cssmin())
+        .pipe(sourcemaps.write('./'))
+        .pipe(gulp.dest('./assets/css'));
+});
+
+// Слежение
+gulp.task('watch', function() {
+    gulp.watch('./src/sass/**/*.{sass,scss}', ['css']);
+});
+
+// Стандартный таск
+gulp.task('default', ['watch', 'js', 'css']);
